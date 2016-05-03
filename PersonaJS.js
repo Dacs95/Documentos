35f5e23189cc26819dc85d5917f9c8233fb500ec
Daniel <bones2995@hotmail.com>
2016-05-03 07:34:47 -0500

    html files

---
+++ PersonaJS.js
@@ -0,0 +1,109 @@
+var clic = 1;
+function d1(){ 
+   if(clic==1){
+   document.getElementById("caja1").style.height = "100px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja1").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d2(){ 
+   if(clic==1){
+   document.getElementById("caja2").style.height = "50px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja2").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d3(){ 
+   if(clic==1){
+   document.getElementById("caja3").style.height = "50px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja3").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d4(){ 
+   if(clic==1){
+   document.getElementById("caja4").style.height = "150px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja4").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d5(){ 
+   if(clic==1){
+   document.getElementById("caja5").style.height = "120px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja5").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d6(){ 
+   if(clic==1){
+   document.getElementById("caja6").style.height = "25px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja6").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d7(){ 
+   if(clic==1){
+   document.getElementById("caja7").style.height = "25px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja7").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d8(){ 
+   if(clic==1){
+   document.getElementById("caja8").style.height = "75px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja8").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d9(){ 
+   if(clic==1){
+   document.getElementById("caja9").style.height = "250px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja9").style.height = "0px";      
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function d10(){ 
+   if(clic==1){
+   document.getElementById("caja10").style.height = "250px";
+   clic = clic + 1;
+   } else{
+       document.getElementById("caja10").style.height = "0px";      
+    clic = 1;
+   }   
+}
